e321abada748a41b623e821ee84d6a39ebf616f4
Anchen Huang <acmhuang@gmail.com>
2019-07-04 18:02:39 +0000

    eslint and fixed up line 5

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,23 +2,22 @@ const request = require('request');
 
 const breedName = process.argv[2];
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=sib`;
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-//When we are requesting over here with the URL, its bring int a string.
 request(url, (error, resp, body) => {
   //console.log(body);
   //console.log(typeof body);
 
-if(error) {
-  return console.log('Failed to request details', error);
-}
+  if (error) {
+    return console.log('Failed to request details', error);
+  }
 
-//This will covert the body retrieved by request from the API url into objects.
+  //This will convert the body retrieved by request from the API url to object;
   const data = JSON.parse(body);
 
-const breed = data[0];
-  if(breed) {
-    console.log(breed.description) 
+  const breed = data[0];
+  if (breed) {
+    console.log(breed.description);
   } else {
     console.log('Failed to find breed ${Siberian}');
   }
